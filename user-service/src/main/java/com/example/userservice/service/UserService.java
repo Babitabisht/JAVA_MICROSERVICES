d7ed6a941d36b9d6e16ee6b59f9c3c536407bfec
babita.bisht <babita.bisht@globallogic.com>
2021-08-13 19:12:46 +0530

    gateway and discovery service added

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -4,11 +4,14 @@ import com.example.userservice.VO.Department;
 import com.example.userservice.VO.ResponseTemplateVO;
 import com.example.userservice.entity.User;
 import com.example.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
+@Slf4j
 public class UserService
 {
     @Autowired
@@ -26,8 +29,9 @@ public class UserService
     }
 
     public ResponseTemplateVO getUserWithDepartment(Long id) {
+        log.info("in get user with department");
         User user = userRepository.findByUserId(id);
-        Department department = restTemplate.getForObject("http://DEPARTMENT_SERVICE/department/" + user.getDepartmentId(), Department.class);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/department/" + user.getDepartmentId(), Department.class);
         ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
         responseTemplateVO.setUser(user);
         responseTemplateVO.setDepartment(department);
